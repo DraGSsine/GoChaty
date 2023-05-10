3a870e0e0f272d7606cf3cf5ced1a5111f426816
yassine <ouchen606@gmail.com>
2023-05-10 16:46:59 +0100

    Small change

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,15 +13,6 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
     },
-    
-      "keyframes": {
-        "shimmer": {
-          "100%": {
-            "transform": "translateX(100%)",
-          },
-        },
-      }
-    
   },
   plugins: [],
 }
